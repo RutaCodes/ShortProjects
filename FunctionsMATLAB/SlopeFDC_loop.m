3024df8bd8859199404339238687c4bdc929067f
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-10-17 13:49:52 -0400

    Update and rename BestRun.m to SlopeFDC_loop.m

---
+++ FunctionsMATLAB/SlopeFDC_loop.m
@@ -0,0 +1,15 @@
+%Ruta Basijokaite
+
+%Input: Simulated (qsyr) and observed (qoyr) stream flow values from one year
+%Output: Slope of flow duration curve for medium flows (33th - 66th percentile)
+
+function[SFDC] = SlopeFDC_loop(qoyr,qsyr)
+
+N = length(qoyr);
+Qob_sort = sort(qoyr,'descend');
+Qsim_sort = sort(qsyr','descend');
+rank = 1:N;
+Ex_p = 100.*(rank/(1+N));
+
+%Slope of flow duration curve - Medium flows
+SFDC = (log(prctile(Qob_sort,66))-log(prctile(Qob_sort,33)))/(0.33-0.66);
