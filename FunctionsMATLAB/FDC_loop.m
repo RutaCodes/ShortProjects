04335ef61252b0cca93a41de6eabb4f23d9d409c
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-10-17 12:16:47 -0400

    Add files via upload

---
+++ FunctionsMATLAB/FDC_loop.m
@@ -0,0 +1,24 @@
+%Variables
+%N - length of yearly flows
+%Q_sort - sorted streamflow
+%rank - ranked streamflows
+%Ex_p - exceedance probability
+%i1 - rank (integer) of specified percentile
+%theta - estimator
+%Q_f - streamflow at specified percentiles
+
+function [Q_f] = FDC_loop(flow,percentiles)
+
+N=length(flow);
+Q_sort=sort(flow,'descend');
+rank=1:N;
+Ex_p=(rank/(1+N));
+
+i1=floor((N+1)*percentiles);
+theta=((N+1)*percentiles-i1);
+
+%Q_f=(1-theta)*Q_sort(i1)+theta*Q_sort(i1+1);
+
+for b=1:length(percentiles)
+    Q_f(b)=(1-theta(b))*Q_sort(i1(b))+theta(b)*Q_sort((i1(b))+1);
+end
