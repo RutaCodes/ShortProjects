b249aca4fc71473eaebcc812dc1aa38bf54f2050
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-05 19:06:52 -0500

    Update digital_root.py

--- CodeWars/digital_root.py
+++ CodeWars/digital_root.py
@@ -5,9 +5,8 @@ Created on Sun Dec  4 22:47:08 2022
 
 @author: rutabasijokaite
 """
-
+# PROBLEM:
 #Digital root is the recursive sum of all the digits in a number.
-
 #Given n, take the sum of the digits of n. If that value has more than one digit, 
 #continue reducing in this way until a single-digit number is produced. The input 
 #will be a non-negative integer.
@@ -18,6 +17,8 @@ Created on Sun Dec  4 22:47:08 2022
 #132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 #493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  --> 1 + 1 = 2
 
+
+# SOLUTION:
 def digital_root(n):
     while (len(str(n))>1):
         nr_sum = 0
