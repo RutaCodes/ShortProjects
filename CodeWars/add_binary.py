cc925e9c41b907083982753ef012ceed59f2902e
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-05 19:07:27 -0500

    Update add_binary.py

--- CodeWars/add_binary.py
+++ CodeWars/add_binary.py
@@ -15,6 +15,7 @@ Created on Fri Dec  2 14:12:37 2022
 #1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 #5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
+
 # SOLUTION:
 def add_binary(a,b):
     return bin(a+b)[2:]
