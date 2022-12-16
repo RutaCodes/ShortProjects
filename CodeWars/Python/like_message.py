1c2557c01f0079781380c43655359e9e2339bf59
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-16 17:49:35 -0500

    Update and rename CodeWars/like_message.py to CodeWars/Python/like_message.py

--- CodeWars/Python/like_message.py
+++ CodeWars/Python/like_message.py
@@ -7,6 +7,7 @@ Created on Fri Dec  2 14:03:12 2022
 """
 
 # PROBLEM:
+
 #You probably know the "like" system from Facebook and other pages. People can "like" blog posts, 
 #pictures or other items. We want to create the text that should be displayed next to such an item.
 #Implement the function which takes an array containing the names of people that like an item. It 
@@ -19,6 +20,7 @@ Created on Fri Dec  2 14:03:12 2022
     
     
 # SOLUTION:
+
 def likes(names):
     if len(names) == 0:
         message = "no one likes this"
