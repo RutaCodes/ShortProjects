efd4d66a6fb13c22d5173f101c9714aa1c974ff5
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-17 20:47:29 -0500

    Update and rename CodeWars/masked_string.py to CodeWars/Python/masked_string.py

--- CodeWars/Python/masked_string.py
+++ CodeWars/Python/masked_string.py
@@ -7,6 +7,7 @@ Created on Fri Dec  2 14:07:30 2022
 """
 
 # PROBLEM:
+
 #Usually when you buy something, you're asked whether your credit card number, phone number 
 #or answer to your most secret question is still correct. However, since someone could look over your 
 #shoulder, you don't want that shown on your screen. Instead, we mask it.
@@ -14,6 +15,7 @@ Created on Fri Dec  2 14:07:30 2022
 
 
 # SOLUTION:
+
 def maskify(cc):
     
     #input - cc - is a string that needs to be masked 
