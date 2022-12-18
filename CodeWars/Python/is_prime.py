45cbf0a44141a3505fbb2c112ff90b017dd756bd
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-18 17:27:54 -0500

    Update is_prime.py

--- CodeWars/Python/is_prime.py
+++ CodeWars/Python/is_prime.py
@@ -18,6 +18,7 @@ Created on Tue Dec  6 15:40:00 2022
 #You can assume you will be given an integer input.
 #You can not assume that the integer will be only positive. You may be given 
 #negative numbers as well ( or 0 ).
+
 #NOTE on performance: There are no fancy optimizations required, but still the most 
 #trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). 
 #Looping all the way up to n, or n/2, will be too slow.
