c911bd0a821f6b3ae79acc355ab90b1223214e0e
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2022-12-27 22:20:32 -0500

    Update toys_for_people.sql

--- CodeWars/SQL/toys_for_people.sql
+++ CodeWars/SQL/toys_for_people.sql
@@ -1,1 +1,23 @@
+--# PROBLEM 
 
+--For this challenge you need to create a simple SELECT statement that will return all columns from the people table, and join to the toys table so that you can return 
+--the COUNT of the toys.
+
+--people table schema
+--id
+--name
+
+--toys table schema
+--id
+--name
+--people_id
+
+--You should return all people fields as well as the toy count as "toy_count".
+
+--# SOLUTION
+
+SELECT p.id, p.name, COUNT(*) toy_count
+FROM people p
+JOIN toys t
+ON t.people_id = p.id
+GROUP BY p.id, p.name
