aff09abc985f1ac44da9b952b8e1817c44fe925a
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-01-04 18:25:51 -0500

    6 kyu

---
+++ CodeWars/SQL/top_customers.sql
@@ -0,0 +1,30 @@
+/*
+# PROBLEM
+
+For this kata we will be using the DVD Rental database.
+
+You are working for a company that wants to reward its top 10 customers with a free gift. You have been asked to generate a simple report that returns the 
+top 10 customers by total amount spent ordered from highest to lowest. Total number of payments has also been requested.
+
+The query should output the following columns:
+customer_id [int4]
+email [varchar]
+payments_count [int]
+total_amount [float]
+
+and has the following requirements:
+only returns the 10 top customers, ordered by total amount spent from highest to lowest
+
+*/
+
+--# SOLUTION
+
+SELECT c.customer_id, c.email, COUNT(*) AS payments_count, SUM(p.amount)::FLOAT AS total_amount
+FROM rental r
+JOIN customer c
+ON r.customer_id = c.customer_id
+JOIN payment p
+ON r.rental_id = p.rental_id
+GROUP BY c.customer_id
+ORDER BY total_amount DESC
+LIMIT 10;
