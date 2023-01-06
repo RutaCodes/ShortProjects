0e54fd5a532788e3f894a8a6a069254a67dee08f
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-01-05 22:26:11 -0500

    8 kyu

---
+++ CodeWars/SQL/easy_ordering.sql
@@ -0,0 +1,18 @@
+/*
+# PROBLEM
+
+Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+
+companies table schema:
+id
+ceo
+motto
+employees
+
+*/
+
+--# SOLUTION
+
+SELECT *
+FROM companies 
+ORDER BY employees DESC;
