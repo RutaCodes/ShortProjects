074b1551e0e48b681be0035c184114a4422a27a1
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-03-24 20:16:28 -0400

    Update miniMaxSum.py

--- CodingChallanges/Python/miniMaxSum.py
+++ CodingChallanges/Python/miniMaxSum.py
@@ -42,41 +42,10 @@ Created on Fri Mar 24 20:07:20 2023
 
 # SOLUTION
 
-import math
-import os
-import random
-import re
-import sys
-#import numpy as np
-
-#
-# Complete the 'miniMaxSum' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-#min_s = 0
-#max_s = 0
 def miniMaxSum(arr):
-    # Write your code here
-    #sum_arr = sum(arr)
     print(sum(arr) - max(arr), sum(arr) - min(arr))
-    
-    #min_v_ind = arr.index(min(arr))
-    #max_v_ind = arr.index(max(arr))
-    #sum_max = np.array(sum(arr[:min_v_ind] + arr[min_v_ind+1:]),dtype=int)
-    #sum_min = np.uint64(sum(arr[:max_v_ind] + arr[max_v_ind+1:]))
-    
-    #max_sum = sum(arr.remove(arr[arr.index(min(arr))]))
-    #min_sum = sum(arr.remove(arr[arr.index(max(arr))]))
-    #min_s = max_s = 0
-    #for i in range(1,len(arr)):
-    #    sum_n = sum(arr[:i] + arr[i+1:])
-    #    if min_s > sum_n: 
-    #        min_s = sum_n
-    #    if max_s < sum_n:
-    #        max_s = sum_n
-    #return sum_max
 
+    
 if __name__ == '__main__':
 
     arr = list(map(int, input().rstrip().split()))
