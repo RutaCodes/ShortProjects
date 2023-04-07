73b661ab912100c3561c35debe615cf076af557d
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-04-06 23:23:30 -0400

    Add files via upload

---
+++ CodingChallanges/Python/ProblemSolving/staircase.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  6 23:17:31 2023
+
+@author: rutabasijokaite
+"""
+
+# This is a staircase of size n. Its base and height are both equal to n. It is drawn using # symbols and spaces. 
+# The last line is not preceded by any spaces.
+
+def staircase(n):
+    for i in range(1,n+1):
+        print(f"{' '*(n - i) + '#'*i}")
+
+def staircase(n):
+    return [[' '*(n - i) + '#'*i] for i in range(1,n+1)]
+
+# Others:
+def staircase(n):
+    for i in range(1,n+1):
+        print(f"{'#'*i:>{n}}")
