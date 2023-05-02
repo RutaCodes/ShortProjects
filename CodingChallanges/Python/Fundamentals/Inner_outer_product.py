23245a135a49679361965378a0150370951e0fc3
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-05-01 20:38:45 -0400

    Update Inner_outer_product.py

--- CodingChallanges/Python/Fundamentals/Inner_outer_product.py
+++ CodingChallanges/Python/Fundamentals/Inner_outer_product.py
@@ -6,9 +6,7 @@ Created on Mon May  1 19:23:31 2023
 @author: rutabasijokaite
 """
 
-
 '''
-
 import numpy
 A = numpy.array([0, 1])
 B = numpy.array([3, 4])
@@ -28,3 +26,23 @@ B = numpy.array(input().split(), int)
 
 print(numpy.inner(A, B)) 
 print(numpy.outer(A, B))
+
+
+
+'''
+The polyval tool evaluates the polynomial at specific value:
+print numpy.polyval(P, x)
+
+You are given the coefficients of a polynomial P
+You task is to find the value of P at point x.
+
+Example: print numpy.polyval([1, -2, 0, 2], 4)
+
+'''
+
+import numpy
+
+P = numpy.array(input().split(), float)
+x = int(input())
+
+print(numpy.polyval(P, x))
