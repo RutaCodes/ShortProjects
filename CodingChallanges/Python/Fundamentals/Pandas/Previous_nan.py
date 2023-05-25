8c6216066088dd4749c8e1219ce16343be566bb1
Ruta Basijokaite <111301407+RutaCodes@users.noreply.github.com>
2023-05-25 14:19:20 -0400

    Update Previous_nan.py

--- CodingChallanges/Python/Fundamentals/Pandas/Previous_nan.py
+++ CodingChallanges/Python/Fundamentals/Pandas/Previous_nan.py
@@ -20,29 +20,11 @@ then return the previous value.
 
 '''
 
-
 # SOLUTION
 
 import pandas as pd
 import numpy as np
 import math
-
-Data_v = {'client_id': [1001,1001,1001,1002,1002,1002,1003,1003],
-        'ranking': [1,2,3,1,2,3,1,2],
-        'value': [1000,np.nan,1200,1500,1250,np.nan,1100,np.nan]}
-Data_d = pd.DataFrame(data=Data_v)
-
-ind_v = np.array(Data_d.loc[Data_d['value'].isnull()].index)
-for i in ind_v:
-    for j in range(1,i+1):
-        if math.isnan(Data_d['value'].loc[i-j]) == False:
-            #Data_d = Data_d.replace(Data_d['value'].loc[i], Data_d['value'].loc[i-j])
-            Data_d.loc[i,'value'] = Data_d['value'].loc[i-j]
-            break
-        
-Data_d = Data_d.sort_values(by=['ranking'])     
-
-
      
 #Example dataframe
 Data_t = pd.DataFrame(data = {'client_id': [1001,1001,1001,1002,1002,1002,1003,1003],
